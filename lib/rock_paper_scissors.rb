29490779e4ba0abf020964828820da9da85ad5f8
Brian Harvey <brian.harv3y@gmail.com>
2020-09-23 14:20:25 -0700

    passes 3 TDD tests

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,11 +1,15 @@
+ #!/usr/bin/env ruby
+
 require('pry')
 
 class RPS 
-  def initialize(str)
-    @input = str
+  def initialize(string)
+    @input = string
     @rock = "rock"
     @paper = "paper"
     @scissors = "scissors"
+    @random = ["rock", "paper", "scissors"]
+    puts 'Enter rock, paper, or scissors:'
   end
 
   def input
@@ -23,22 +27,53 @@ class RPS
   def scissors
     @scissors
   end
-end     
 
-
-
-
-
-class CPU < RPS
-  def initialize
-    @random
+  def input=(input)
+    @input = input
   end
 
   def random
     @random
+  end  
+
+  def get_random()
+    cpu_move = @random.shuffle
+    puts "#{cpu_move[0]}"
+    return cpu_move[0]
   end
-end  
+
+  def get_move()
+    puts player_input = @input
+    return player_input
+  end
+  
+  def game_win(player, cpu)
+    player = get_move()
+    cpu = get_random()
+      if (player === "rock" && cpu === "scissors")
+        return "you win"
+      elsif (player === "rock" && cpu === "rock")
+        return "draw" 
+      elsif (player === "rock" && cpu === "paper")
+        return "you lose" 
+      elsif (player === "paper" && cpu === "rock")
+        return "you win"
+      elsif (player === "paper" && cpu === "paper")
+        return "draw"
+      elsif (player === "paper" && cpu === "scissors")
+        return "you lose"
+      elsif (player === "scissors" && cpu === "paper")
+        return "you win"
+      elsif (player === "scissors" && cpu === "scissors")
+        return "draw"
+      elsif (player === "scissors" && cpu === "rock")
+        return "you lose"
+      else (player != "scissors" || "rock" || "paper")
+        return "Please enter rock, paper, or scissors."
+      end
+  end
+end     
 
 
 
-
+  
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,7 +4,28 @@ require('rock_paper_scissors')
 
 describe('RPS#initializes') do
   it('initializes RPS class') do
-    test = RPS.new("rock")
-    expect(test.input).to(eq("rock"))
+    new_game = RPS.new("rock")
+    expect(new_game.input).to(eq("rock"))
+  end
+end
+
+# describe('#get_random') do
+#   it('retrieves the 0 index of a randomized array of strings') do
+#     new_game = RPS.new("rock")
+#     expect(new_game.get_random()).to(eq("rock" || "paper" || "scissors"))
+#   end
+# end
+
+describe('#get_move') do
+  it('retrieves the input from player and returns string') do
+    new_game = RPS.new("rock")
+    expect(new_game.get_move()).to(eq("rock"))
+  end
+end
+
+describe('#game_win') do
+  it('determines winner of round') do
+    new_game = RPS.new("rock")
+    expect(new_game.game_win("rock", "scissors")).to(eq("you win"))
   end
 end
